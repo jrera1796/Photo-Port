f1e6fee3a435839809768558549dec88d168fd6f
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-02-08 09:47:54 -0800

    About and Nav components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Nav(){
+  function categorySelected(name){
+    console.log(`hello ${name}`)
+  }
+  const categories =[
+    {
+      name: "Commercial",
+      description:
+      "Photos of grocery stores, food trucks, and other commercial projects",
+  },
+  { name: "Portraits", description: "Portraits of people in my life" },
+  { name: "Food", description: "Delicious delicacies" },
+  {
+    name: "Landscape",
+    description: "fields, farmhouses, waterfalls, and the beauty of nature",
+  },
+  ]
+  return (
+    <header className="flex-row">
+      <h2>
+        <a href="/">
+          <span role="img" arial-label="camera">📸</span>Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">
+              About Me
+            </a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+          <li className="mx-1" key={category.name}>
+            <span onClick={()=>categorySelected(category.name)}>
+              {category.name}
+              </span>
+          </li>
+  ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
